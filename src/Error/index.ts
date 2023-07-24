317b0d25da1e30c5722875c1ab7260ade908bbb2
mimamch <mimamch28@gmail.com>
2023-07-24 12:37:42 +0700

    fix: WhatsappError class intanceof return false

--- src/Error/index.ts
+++ src/Error/index.ts
@@ -1,5 +1,11 @@
 export class WhatsappError extends Error {
   constructor(message: string) {
     super(message);
+    this.name = "WhatsappError";
+    Object.setPrototypeOf(this, WhatsappError.prototype);
+  }
+
+  static isWhatsappError(error: any): error is WhatsappError {
+    return error instanceof WhatsappError || error instanceof Error;
   }
 }
